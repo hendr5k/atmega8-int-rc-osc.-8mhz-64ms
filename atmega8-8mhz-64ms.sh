d6af5d95684a614275969da01e00cc3972fd784b
hendr5k <hendrik.schmitz03.hs@gmail.com>
2018-09-28 19:42:46 +0200

    Add files via upload

---
+++ atmega8-8mhz-64ms.sh
@@ -0,0 +1,18 @@
+echo Programmer Anschließen
+sleep 5
+echo flash startet in 5 sek
+sleep 1
+echo 5
+sleep 1
+echo 4
+sleep 1
+echo 4
+sleep 1
+echo 3
+sleep 1
+echo 2
+sleep 1
+echo 1
+sleep 1
+echo 0
+avrdude -c "PROGRAMMER NAME" -P usb -p atmega8 -U lfuse:w:0xe4:m -U hfuse:w:0xd9:m 
